25263eee63afac65fc73134edd4310cc1598ef4a
Mikel Bishop <mdrew8@gmail.com>
2020-08-22 17:01:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,7 @@ def speak_to_grandma(string)
     return 'I LOVE YOU TOO PUMPKIN!'
     elsif string == string.upcase
     return 'NO, NOT SINCE 1938!'
+  else
+    return 'HUH?! SPEAK UP, SONNY!'
 end
 end
